f18078d4b0c62fc8cee953e15d703086239355c4
Ryan Loughry <thecoderanger5280@icloud.com>
2022-07-08 10:26:11 -0600

    Change vars, add puts

--- variables.rb
+++ variables.rb
@@ -6,3 +6,11 @@ height_feet = 6.25
 miles_biked = 3.5
 loves_coding = true
 loves_writing = false
+
+first_name = "Han"
+last_name = "Solo"
+
+puts first_name
+puts last_name
+puts favorite_number
+puts miles_biked
